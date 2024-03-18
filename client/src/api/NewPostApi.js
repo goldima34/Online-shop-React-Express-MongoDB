b7981f6c891f47b82d20f2c948620a39ae72b657
dima <golovaschukdima@gmail.com>
2024-03-18 21:42:12 +0200

    nova poshta api

--- client/src/api/NewPostApi.js
+++ client/src/api/NewPostApi.js
@@ -1,15 +1,34 @@
 import axios from "axios";
 
-const apiKey = "";
+const apiKey = "a8ce1029e7856b8a14e6b172318a0f31";
 
-export const NewPostGetRegion = async () => {
+export const NewPostGetCity = async (city) => {
   try {
     const response = await axios.post("https://api.novaposhta.ua/v2.0/json/", {
       apiKey: apiKey,
       modelName: "Address",
-      calledMethod: "getSettlementAreas",
+      calledMethod: "getCities",
       methodProperties: {
         Ref: "",
+        FindByString: city,
+      },
+    });
+
+    // Обработка полученных данных
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+export const NewPostGetWarehouses = async (city) => {
+  try {
+    const response = await axios.post("https://api.novaposhta.ua/v2.0/json/", {
+      apiKey: apiKey,
+      modelName: "Address",
+      calledMethod: "getWarehouses",
+      methodProperties: {
+        CityName: city,
       },
     });
 
--- client/src/pages/BilingDetails.js
+++ client/src/pages/BilingDetails.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import style from "../styles/BilingDetails.module.css";
-import { NewPostGetRegion } from "../api/NewPostApi";
+import { NewPostGetCity, NewPostGetRegion, NewPostGetWarehouses } from "../api/NewPostApi";
+import { Select, Button } from "antd";
 export const BilingDetails = () => {
-  const [data, setData] = useState();
+  const [regions, setRegions] = useState();
+  const [selectedCity, setSelectedCity] = useState();
   const [loading, setLoading] = useState(true);
+  const [searchCityString, setSearchCityString] = useState("");
+  const [warehouses, setWarehouses] = useState([])
+  const [selectedWarehouses, setSelectedWarehouses] = useState();
 
   useEffect(() => {
-    NewPostGetRegion().then((data) => setData(data));
+    NewPostGetCity(searchCityString).then((data) => setRegions(data));
     setTimeout(() => {
       setLoading(false);
     }, 500);
-  });
+  }, [searchCityString]);
+
+  const getWarehouse = (selectedCity) => {
+    NewPostGetWarehouses(selectedCity).then((data) => console.log(data));
+  };
 
   if (loading) {
     return <div></div>;
@@ -41,16 +50,46 @@ export const BilingDetails = () => {
           </div>
         </div>
         <div className={style.BilingSuccessContainer}>
-          <select id="billingOptions">
-            <option>
-              Виберіть область:
-            </option>
-            {data.map((item) => (
-              <option key={item.id} value={item.value}>
+          <Select
+            mode="single"
+            value={selectedCity}
+            showSearch={true}
+            onChange={(value) => {
+              setSelectedCity(value);
+              getWarehouse(value);
+            }}
+            onSearch={(value) => setSearchCityString(value)} // Обновляем значение строки поиска при изменении
+            style={{ width: 120 }}
+            loading={loading}
+            filterOption={false} // Отключаем автоматический фильтр опций
+            notFoundContent={loading ? "Loading..." : "No data found"} // Отображаем сообщение при отсутствии данных
+          >
+            {regions.map((item) => (
+              <Select.Option key={item.Ref} value={item.Description}>
                 {item.Description}
-              </option>
+              </Select.Option>
             ))}
-          </select>
+          </Select>
+          <Select
+            mode="single"
+            value={selectedCity}
+            showSearch={true}
+            onChange={(value) => {
+              setSelectedCity(value);
+              getWarehouse(value);
+            }}
+            onSearch={(value) => setSelectedWarehouses(value)} // Обновляем значение строки поиска при изменении
+            style={{ width: 120 }}
+            loading={loading}
+            filterOption={false} // Отключаем автоматический фильтр опций
+            notFoundContent={loading ? "Loading..." : "No data found"} // Отображаем сообщение при отсутствии данных
+          >
+            {regions.map((item) => (
+              <Select.Option key={item.Ref} value={item.Description}>
+                {item.Description}
+              </Select.Option>
+            ))}
+          </Select>
         </div>
       </div>
     </div>
