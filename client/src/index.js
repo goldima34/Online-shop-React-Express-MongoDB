1f917cd60733137a40dc49445e7d9b24f5f7c276
dima <golovaschukdima@gmail.com>
2024-04-02 08:25:46 +0300

    admin panel update

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,24 @@
-import React, { createContext } from "react";
+import React, {createContext} from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.js";
 import UserStore from "./store/UserStore";
 import ItemStore from "./store/ItemStore";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 export const Context = createContext(null);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Context.Provider
-    value={{
-      userStore: new UserStore(),
-      item: new ItemStore(),
-    }}
-  >
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </Context.Provider>
+    <Context.Provider
+        value={{
+            userStore: new UserStore(),
+            item: new ItemStore(),
+        }}
+    >
+        <React.StrictMode>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </React.StrictMode>
+    </Context.Provider>
 );
--- server/index.js
+++ server/index.js
@@ -13,12 +13,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
+app.use(cors({
+    credentials: true, origin: process.env.CLIENT_URL,
+}));
 app.use(fileUpload());
 app.use(express.static("static"));
 app.use("/api", router);
@@ -26,17 +23,14 @@ app.use(ErrorMidleware);
 
 //process.env.CLIENT_URL
 const start = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://admin:root@cluster0.avjsghl.mongodb.net/",
-      {}
-    );
-    app.listen(PORT, () => {
-      console.log(`Server started on PORT = ${PORT}`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
+    try {
+        await mongoose.connect("mongodb+srv://admin:root@cluster0.avjsghl.mongodb.net/", {});
+        app.listen(PORT, () => {
+            console.log(`Server started on PORT = ${PORT}`);
+        });
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 start();
